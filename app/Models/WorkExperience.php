dd544818b60ac509f58ed739897cef1bba980bde
Hemant Kumawat <hemantkumawat275@gmail.com>
2022-01-26 20:58:31 +0530

    Database Settings Configured

--- app/Models/WorkExperience.php
+++ app/Models/WorkExperience.php
@@ -11,11 +11,6 @@ class WorkExperience extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $connection = 'mongodb';
-    protected $collection = 'work_experiences';
-
-    protected $table = 'work_experiences';
-
     protected $fillable = [
         'user_id',
         'company',
